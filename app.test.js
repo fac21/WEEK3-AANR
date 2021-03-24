552aa5b0095a969566b91f11997cfe051a840d75
Amy Kuner <amykuner1@gmail.com>
2021-03-24 15:10:42 +0000

    First two tests added

--- app.test.js
+++ app.test.js
@@ -1,11 +1,16 @@
 // Add tasks to a list so that I can keep track of them
 test("Submitting a new task adds it to the list", () => {
-  // test goes here
+  let input = "ham"
+  let taskNumExp = document.getElementsByClassName("tasks").length + 1;
+  equal(addTask(input).length, taskNumExp);
+  equal(addTask(input)[addTask(input).length - 1].innerHTML, input);
 });
 
 // Check things off my list so that I can see what I’ve done
 test("Checking an entry marks it as complete", () => {
-  // test goes here
+  let tick = (document.getElementById("myCheck"));
+  equal(tick.checked, true);
+  equal(tick.parentElement.classList.contains("completed"), true);
 });
 
 // Delete things from the list if I don’t need to do them anymore
@@ -16,5 +21,13 @@ test("Deleting an entry removes it from the list", () => {
 
 // Filter out completed to-dos from my list so that I can focus on what’s left to do
 test("Toggling the filter hides completed tasks from the list", () => {
-  // test goes here
+  
 });
+
+function addTask(){
+  return document.getElementsByClassName("tasks") //this should return an array with every list element
+}
+
+function check(){
+
+}
