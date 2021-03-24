3a35e7a7d40f9d527bfd7b5f0fcfe46ecf801360
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-24 14:51:52 +0000

    adding test for deleted items
    Relates #3

--- app.test.js
+++ app.test.js
@@ -11,10 +11,24 @@ test("Checking an entry marks it as complete", () => {
 // Delete things from the list if I don’t need to do them anymore
 
 test("Deleting an entry removes it from the list", () => {
-  // test goes here
+  let expected = true;
+  let listItemId = "task1"; 
+  removeToDoItem(listItemId);
+  let ul = Array.from(document.querySelector("ul"));
+  let actual = ul.every((e) => e.id != ListItemId);
+  equal(actual, expected)
+});
+
+test("Deleting an entry removes it from the list", () => {
+  let expected = 2; 
+  let listItemId = "task1";
+  removeToDoItem(listItemId);
+  let actual = document.querySelector("ul").length;
+  equal(actual, expected)
 });
 
 // Filter out completed to-dos from my list so that I can focus on what’s left to do
 test("Toggling the filter hides completed tasks from the list", () => {
   // test goes here
 });
+
