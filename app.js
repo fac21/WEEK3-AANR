a535a9a272cb0c3058676202dcdab60f17fc7480
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-25 12:17:06 +0000

    altering test to get test function working, addTask function now passing tests
    Relates #1

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 if ("content" in document.createElement("template")) {
-  function addTask(event) {
-    event.preventDefault();
-
+  function addTask(input) {
     let template = document.getElementById("task_item_template");
     let clone = template.content.cloneNode(true);
 
     let ul = document.getElementById("ul");
     ul.appendChild(clone);
 
-    let formData = document.querySelector("#task").value;
-    console.log(formData);
+    console.log(input);
 
     let templateListItems = document.querySelectorAll("li");
     console.log(templateListItems);
     templateListItems[
       templateListItems.length - 1
-    ].childNodes[2].textContent = formData;
+    ].childNodes[2].textContent = input;
 
     document.getElementById("task").value = "";
+
+    //console.log(document.querySelectorAll("li"))
+    return document.querySelectorAll("li");
   }
 
   document
     .querySelector("input[type='submit']")
-    .addEventListener("click", addTask);
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      let formData = document.querySelector("#task").value;
+      addTask(formData);
+    });
 }
--- app.test.js
+++ app.test.js
@@ -1,19 +1,21 @@
-// // Add tasks to a list so that I can keep track of them
-// test("Submitting a new task adds it to the list", () => {
-//   let input = "ham"
-//   let taskNumExp = document.getElementsByClassName("tasks").length + 1;
-//   equal(addTask(input).length, taskNumExp);
-//   equal(addTask(input)[addTask(input).length - 1].innerHTML, input);
-// });
+// Add tasks to a list so that I can keep track of them
+test("Submitting a new task adds it to the list", () => {
+  let input = "ham"
+  let taskNumExp = document.getElementsByClassName("tasks").length + 1;
+  let actual = addTask(input);
+  console.log(actual)
+  equal(actual.length, taskNumExp);
+  equal(actual[actual.length - 1].childNodes[2].textContent, input);
+});
 
-// // Check things off my list so that I can see what I’ve done
+// Check things off my list so that I can see what I’ve done
 // test("Checking an entry marks it as complete", () => {
 //   //tick = (document.getElementById("myCheck"));
 //   equal(check("alistitem").checked, true);
 //   equal(check("alistitem").parentElement.classList.contains("completed"), true);
 // });
 
-// // Delete things from the list if I don’t need to do them anymore
+// Delete things from the list if I don’t need to do them anymore
 
 // test("Deleting an entry removes it from the list", () => {
 //   let expected = true;
