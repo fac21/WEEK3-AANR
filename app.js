69e5743e9533bdadba6820b90906ebf047e8d841
Amy Kuner <amykuner1@gmail.com>
2021-03-25 16:47:02 +0000

    generate id function added

--- app.js
+++ app.js
@@ -11,9 +11,18 @@ if ("content" in document.createElement("template")) {
     templateListItems[
       templateListItems.length - 1
     ].childNodes[3].textContent = input;
+    console.log(templateListItems)
 
     document.getElementById("task__input").value = "";
 
+    templateListItems[
+      templateListItems.length - 1
+    ].childNodes[1].id = genID();
+
+    templateListItems[
+      templateListItems.length - 1
+    ].childNodes[3].htmlFor = genID();
+    
     return document.querySelectorAll("li");
   }
 
@@ -50,3 +59,14 @@ ul.addEventListener("click", (event) => {
     check(item);
   }
 });
+
+function genID(){
+  let templateListItems = document.querySelectorAll("li");
+  if (templateListItems.length == 1)return 'task-1';
+  else if (templateListItems.length > 1){
+    let base = templateListItems[templateListItems.length - 2].childNodes[1].id;
+    let taskNum = parseInt(base.slice(-1)) + 1;
+    let id = base.slice(0, -1) + taskNum;
+    return id;
+  }
+}
