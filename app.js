6d4e8a262d2dd3c18a8d23edea22e571e076e79b
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-25 11:19:38 +0000

    adding correct formatting to js file
    Relates #1

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-
-if ('content' in document.createElement('template')) {
-
-function addTask(event){
+if ("content" in document.createElement("template")) {
+  function addTask(event) {
     event.preventDefault();
-  
-let template = document.getElementById("task_item_template");
-let clone = template.content.cloneNode(true);
 
+    let template = document.getElementById("task_item_template");
+    let clone = template.content.cloneNode(true);
 
-let ul = document.getElementById("ul");
-ul.appendChild(clone);
+    let ul = document.getElementById("ul");
+    ul.appendChild(clone);
 
-let formData = document.querySelector("#task").value;
-console.log(formData)
+    let formData = document.querySelector("#task").value;
+    console.log(formData);
 
+    let templateListItems = document.querySelectorAll("li");
+    console.log(templateListItems);
+    templateListItems[
+      templateListItems.length - 1
+    ].childNodes[2].textContent = formData;
 
-let templateListItems = document.querySelectorAll("li");
-console.log(templateListItems)
-templateListItems[templateListItems.length - 1].childNodes[2].textContent = formData;
+    document.getElementById("task").value = "";
+  }
 
-document.getElementById("task").value = "";
-
-        }
-
-document.querySelector("input[type='submit']").addEventListener("click", addTask)
-
-
-    }
+  document
+    .querySelector("input[type='submit']")
+    .addEventListener("click", addTask);
+}
