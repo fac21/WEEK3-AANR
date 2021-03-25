eb1a9c801d40e881d5b2c5d0e880ae5a294452c1
designaky <designaky@gmail.com>
2021-03-25 15:26:00 +0000

    Clearing the code from console.log
    Closes #3

--- app.js
+++ app.js
@@ -1,58 +1,52 @@
 if ("content" in document.createElement("template")) {
-	function addTask(input) {
-		let template = document.getElementById("task_item_template");
-		let clone = template.content.cloneNode(true);
+  function addTask(input) {
+    let template = document.getElementById("task_item_template");
+    let clone = template.content.cloneNode(true);
 
-		let ul = document.getElementById("ul");
-		ul.appendChild(clone);
+    let ul = document.getElementById("ul");
+    ul.appendChild(clone);
 
-		console.log(input);
+    let templateListItems = document.querySelectorAll("li");
 
-		let templateListItems = document.querySelectorAll("li");
-		console.log(templateListItems);
-		templateListItems[
-			templateListItems.length - 1
-		].childNodes[3].textContent = input;
+    templateListItems[
+      templateListItems.length - 1
+    ].childNodes[3].textContent = input;
 
-		document.getElementById("task__input").value = "";
+    document.getElementById("task__input").value = "";
 
-		//console.log(document.querySelectorAll("li"))
-		return document.querySelectorAll("li");
-	}
+    return document.querySelectorAll("li");
+  }
 
-	document
-		.querySelector("input[type='submit']")
-		.addEventListener("click", (event) => {
-			event.preventDefault();
-			let formData = document.querySelector("#task__input").value;
-			addTask(formData);
-		});
+  document
+    .querySelector("input[type='submit']")
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      let formData = document.querySelector("#task__input").value;
+      if (formData !== "") addTask(formData);
+    });
 }
 
 const deleteButtons = document.querySelectorAll(".task__delete-Btn");
 const ul = document.querySelector("ul");
-console.log(ul);
 
 function check(item) {
-	// console.log(item);
-	item.parentElement.classList.toggle("completed");
-	// console.log(item.checked);
-	// console.log(item.classList);
-	return item;
+  item.parentElement.classList.toggle("completed");
+
+  return item;
 }
 
 function removeToDoItem(parent) {
-	ul.removeChild(parent);
-	return ul;
+  ul.removeChild(parent);
+  return ul;
 }
 
 ul.addEventListener("click", (event) => {
-	const item = event.target;
-	if (item.classList.contains("task__delete-Btn")) {
-		const parent = item.parentElement;
-		removeToDoItem(parent);
-	}
-	if (item.classList.contains("checkbox")) {
-		check(item);
-	}
+  const item = event.target;
+  if (item.classList.contains("task__delete-Btn")) {
+    const parent = item.parentElement;
+    removeToDoItem(parent);
+  }
+  if (item.classList.contains("checkbox")) {
+    check(item);
+  }
 });
--- app.test.js
+++ app.test.js
@@ -1,41 +1,38 @@
 // Add tasks to a list so that I can keep track of them
-// test("Submitting a new task adds it to the list", () => {
-//   let input = "ham"
-//   let taskNumExp = document.getElementsByClassName("tasks").length + 1;
-//   let actual = addTask(input);
-//   console.log(actual)
-//   equal(actual.length, taskNumExp);
-//   equal(actual[actual.length - 1].childNodes[2].textContent, input);
-// });
+test("Submitting a new task adds it to the list", () => {
+  let input = "ham";
+  let taskNumExp = document.getElementsByClassName("tasks").length + 1;
+  let actual = addTask(input);
+  equal(actual.length, taskNumExp);
+  equal(actual[actual.length - 1].childNodes[3].textContent, input);
+});
 
 //Check things off my list so that I can see what I’ve done
 
-// test("Checking an entry marks it as complete", () => {
-//   //tick = (document.getElementById("myCheck"));
-//   let checkboxInput = document.querySelector("input[type='checkbox']")
-//   checkboxInput.checked = true;
-//   let checkedFn = check(checkboxInput);
-//   console.log(checkedFn);
-//   equal(checkedFn.checked, true);
-//   equal(checkedFn.parentElement.classList.contains("completed"), true);
-// });
-
-// test("Deleting an entry removes it from the list", () => {
-//   let expected = true;
-//   let listItemId = document.querySelector("li")
-//   removeToDoItem(listItemId);
-//   let ul = Array.from(document.querySelector("ul"));
-//   let actual = ul.every((e) => e.id != ListItemId);
-//   equal(actual, expected);
-// });
+test("Checking an entry marks it as complete", () => {
+  //tick = (document.getElementById("myCheck"));
+  let checkboxInput = document.querySelector("input[type='checkbox']");
+  checkboxInput.checked = true;
+  let checkedFn = check(checkboxInput);
+  equal(checkedFn.checked, true);
+  equal(checkedFn.parentElement.classList.contains("completed"), true);
+});
 
 test("Deleting an entry removes it from the list", () => {
-  let expected = Array.from(document.querySelectorAll("li")).length -1;
-  let listItemId = document.querySelector("li")
+  let expected = true;
+  let listItemId = document.querySelector("li");
   removeToDoItem(listItemId);
-  console.log(expected)
-  let actual =Array.from(document.querySelectorAll("li")).length 
-  
+  let ul = Array.from(document.querySelector("ul"));
+  let actual = ul.every((e) => e.id != ListItemId);
+  equal(actual, expected);
+});
+
+test("Deleting an entry removes it from the list", () => {
+  let expected = Array.from(document.querySelectorAll("li")).length - 1;
+  let listItemId = document.querySelector("li");
+  removeToDoItem(listItemId);
+  let actual = Array.from(document.querySelectorAll("li")).length;
+
   equal(actual, expected);
 });
 
@@ -56,13 +53,10 @@ test("Integration test", () => {
 
   const toDoList = Array.from(document.querySelectorAll(".task"));
 
-  console.log(toDoList);
-
   let expected = true;
   let actual = toDoList.some(
     (todo) => todo.childNodes[3].textContent === testText
   );
-
 
   // Delete part for next time
   equal(actual, expected);
