a3998404d2648d4c52098c8df3b3a345646d1f73
Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
2021-03-26 11:40:45 +0000

    made filter button clearer and got rid of extra spaces in the files

--- test-helpers.js
+++ test-helpers.js
@@ -1,26 +1,25 @@
 const equal = (actual, expected, message) => {
-if (actual === expected) {
+  if (actual === expected) {
     const defaultMessage = `Expected ${expected} and received ${actual}`;
     console.info("Pass: " + (message || defaultMessage));
-} else {
+  } else {
     const defaultMessage = `Expected ${expected} but received ${actual} instead`;
-    console.error(`Fail: ${message || defaultMessage}`);  
-
-    }
-}
+    console.error(`Fail: ${message || defaultMessage}`);
+  }
+};
 
 const notEqual = (actual, expected, message) => {
-    if (actual !== expected) {
-        const defaultMessage = `${expected} is different to ${actual}`;
-        console.info("Pass: " + (message || defaultMessage));
-      } else {
-        const defaultMessage = `${expected} is the same as ${actual}`;
-        console.error("Fail: " + (message || defaultMessage));
-      }
-    }
+  if (actual !== expected) {
+    const defaultMessage = `${expected} is different to ${actual}`;
+    console.info("Pass: " + (message || defaultMessage));
+  } else {
+    const defaultMessage = `${expected} is the same as ${actual}`;
+    console.error("Fail: " + (message || defaultMessage));
+  }
+};
 
 const test = (name, testFunction) => {
-    console.group(name);
-    testFunction();
-    console.groupEnd(name);
-}
+  console.group(name);
+  testFunction();
+  console.groupEnd(name);
+};
